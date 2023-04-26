5fd748d48bbfa412225d7989b96c2c6922af719f
Kantiyapilad <62011212027@msu.ac.th>
2023-04-26 17:28:31 +0700

    login เสร็จแล้ว

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,32 +1,33 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Convert as amins , Admin } from './admin.model';
 import { DataService } from 'src/app/service/data.service';
+import { Router } from '@angular/router';
+import { Admin } from '../model/admin.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent {
-  admin : Admin | undefined;
-  d : any;
   // const username = document.getElementById("username").value;
-  constructor(private dataService : DataService ,private http : HttpClient){
-    this.http.get(this.dataService.apiEndpoint + "/admin").subscribe((data : any) => {
-      this.admin = amins.toAdmin(JSON.stringify(data));
-      this.d = this.admin.records.length;
-   });
-  }
+  constructor(private dataService : DataService ,private http : HttpClient, private router: Router){}
   findAdmin(email : string , password : string){
-    console.log(email + " "+ password + " " + this.d);
-    for(let i = 0 ; i < this.d ; i++){
-      if(email == this.admin?.records[i].email && password == this.admin.records[i].password ){
-          window.location.href = 'home';
+   // console.log(email + " "+ password);
+
+
+    // https://anihmsu.comsciproject.net/anihmsu/api.php/records/admin?filter=email,eq,6211@msu.ac.th&filter=password,eq,1111111&exclude=password
+
+    this.http.get(this.dataService.apiEndpoint + "/admin?filter=email,eq,"+email+"&filter=password,eq,"+password +"&exclude=password")
+    .subscribe((data : any)=>{
+      let response = data as Admin;
+      if (response.records.length == 1){
+        this.router.navigateByUrl('/home');
+      }else{
+        console.log('Login failed');
+
       }
-      else{
-        console.log("NOOOOOOOO");
-      }
+    });
     }
-  }
 }
---
+++ src/app/model/admin.model.ts
@@ -0,0 +1,13 @@
+
+
+export interface Admin {
+  records: Record[];
+}
+
+export interface Record {
+  id_admin: number;
+  email:    string;
+  name:     string;
+}
+
+
