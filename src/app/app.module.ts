6db242b8b68a146f39e951d82a601bd8031683ce
P <phiraphat.kn@gmail.com>
2023-05-03 16:30:17 +0700

    update identify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import {MatDialogModule} from '@angular/material/dialog';
 import { PersonnelNewComponent } from './personnel-new/personnel-new.component';
 import { PersonnelEditComponent } from './personnel-edit/personnel-edit.component';
 import { PopupComponent } from './home/popup/popup.component';
+import { IdentifyComponent } from './menutoober/identify/identify.component';
 
 const appRoutes: Routes = [
   {path:'',component:LoginComponent},
@@ -32,6 +33,8 @@ const appRoutes: Routes = [
   {path:'personnel-new',component:PersonnelNewComponent},
   {path: 'popup',component:PopupComponent} ,
   {path:'vision-mission',component:VisionMissionComponent},
+  {path:'identify',component:IdentifyComponent},
+
 ];
 
 @NgModule({
@@ -49,6 +52,7 @@ const appRoutes: Routes = [
     PopupComponent,
 
     VisionMissionComponent,
+      IdentifyComponent,
 
 
 
