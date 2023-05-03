4987118ff9705a5451872993521b4fc88ec6739c
P <phiraphat.kn@gmail.com>
2023-05-04 00:05:47 +0700

    update edit Identify

--- src/app/edit-identify/edit-identify.component.ts
+++ src/app/edit-identify/edit-identify.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DataService } from '../service/data.service';
+import { HttpClient } from '@angular/common/http';
+import { IdentifyData } from '../model/identify.model';
 
 @Component({
   selector: 'app-edit-identify',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-identify.component.scss']
 })
 export class EditIdentifyComponent {
+  response={} as IdentifyData;
+  base64 : any;
+  filename : any;
+  constructor(private data : DataService , private http: HttpClient){
+      http.get("https://anihmsu.comsciproject.net/anihmsu/api.php/records/identify?join=image_identify")
+      .subscribe((data : any) => {
+        console.log(data);
+        this.response = data as IdentifyData;
+  });
+  }
+  getFile(files : FileList){
+    let file = files.item(0);
+    this.filename = file?.name;
+    let reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = () => {
+      this.base64 = reader.result;
+      console.log(this.base64);
+    }
 
+    //location.reload();
+
+  }
 }
