2ff68145f5cf6e780647a04dabc08aa07466282f
Kantiyapilad <62011212027@msu.ac.th>
2023-05-29 21:01:18 +0700

    .

--- src/app/menutoober/department/department.component.ts
+++ src/app/menutoober/department/department.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AngularEditorModule } from '@kolkov/angular-editor';
 import { DataVetAnimal } from 'src/app/model/DataVetAnimal.model';
 import { DataService } from 'src/app/service/data.service';
 
@@ -11,6 +12,49 @@ import { DataService } from 'src/app/service/data.service';
 export class DepartmentComponent {
   base64 : any;
   response = {} as DataVetAnimal;
+
+  ///wysiwyg
+  htmlContent = '';
+  htmlContent1 = '';
+  faculty_info : any;
+
+  editorConfig: AngularEditorModule = {
+    editable: true,
+      spellcheck: true,
+      height: 'auto',
+      minHeight: '0',
+      maxHeight: 'auto',
+      width: 'auto',
+      minWidth: '0',
+      translate: 'yes',
+      enableToolbar: true,
+      showToolbar: true,
+      placeholder: 'Enter text here...',
+      defaultParagraphSeparator: '',
+      defaultFontName: '',
+      defaultFontSize: '',
+      fonts: [
+        {class: 'arial', name: 'Arial'},
+        {class: 'times-new-roman', name: 'Times New Roman'},
+
+      ],
+      customClasses: [
+      {
+        name: 'quote',
+        class: 'quote',
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: 'titleText',
+        class: 'titleText',
+        tag: 'h1',
+      },
+    ],
+};
+///
   constructor(private data : DataService , private http: HttpClient){
     http.get("https://anihmsu.comsciproject.net/anihmsu/api.php/records/information_anihmsu")
     .subscribe((data : any) => {
@@ -19,6 +63,24 @@ export class DepartmentComponent {
 });
 }
 
+////wysiwyg
+ngOnInit(): void {
+  let url = this.data.apiEndpoint + '/information_anihmsu';
+  this.http.get(url, {observe : 'response'}).subscribe(
+    {
+      next: (data :any)=>{
+        this.faculty_info = data.body.records[0];
+        this.htmlContent = this.faculty_info.department_structure_th;
+        this.htmlContent1 = this.faculty_info.department_structure_en;
+        console.log(this.faculty_info.id_anihmsu);
+      },
+      error: (err)=>{
+        console.log(err);
+      }
+    }
+  )
+}
+////
 getFile(files : FileList){
     let reader = new FileReader();
     reader.readAsDataURL(files[0]);
@@ -27,14 +89,14 @@ getFile(files : FileList){
       console.log(this.base64);
     }
 }
- updateImg(idx:number,department_structure_th:string,department_structure_en:string){
+ updateImg(){
   let jsonObj ={
-    department_structure_th : department_structure_th,
-    department_structure_en : department_structure_en,
+    department_structure_th : this.htmlContent,
+    department_structure_en : this.htmlContent1,
     img_department_structure : this.base64
   }
   if(confirm("ยืนยันการเเก้ไข") == true){
-    this.http.put(this.data.apiEndpoint+"/information_anihmsu/"+idx,jsonObj,
+    this.http.put(this.data.apiEndpoint+"/information_anihmsu/"+this.faculty_info.id_anihmsu,jsonObj,
     {observe:'response'}).subscribe((response : any)=>
     {console.log(JSON.stringify(response.status));
       console.log(JSON.stringify(response.body));
--- src/app/menutoober/vmsp/vmsp.component.ts
+++ src/app/menutoober/vmsp/vmsp.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AngularEditorModule } from '@kolkov/angular-editor';
 import { DataVetAnimal } from 'src/app/model/DataVetAnimal.model';
 import { DataService } from 'src/app/service/data.service';
 
@@ -12,6 +13,55 @@ export class VMSPComponent {
   response = {} as DataVetAnimal;
   base64 : any;
   filename : any;
+
+  ///wysiwyg
+  htmlContent = '';
+  htmlContent1 = '';
+  htmlContent2 = '';
+  htmlContent3 = '';
+  htmlContent4 = '';
+  htmlContent5 = '';
+  htmlContent6 = '';
+  htmlContent7 = '';
+  faculty_info : any;
+
+  editorConfig: AngularEditorModule = {
+    editable: true,
+      spellcheck: true,
+      height: 'auto',
+      minHeight: '0',
+      maxHeight: 'auto',
+      width: 'auto',
+      minWidth: '0',
+      translate: 'yes',
+      enableToolbar: true,
+      showToolbar: true,
+      placeholder: 'Enter text here...',
+      defaultParagraphSeparator: '',
+      defaultFontName: '',
+      defaultFontSize: '',
+      fonts: [
+        {class: 'arial', name: 'Arial'},
+        {class: 'times-new-roman', name: 'Times New Roman'},
+
+      ],
+      customClasses: [
+      {
+        name: 'quote',
+        class: 'quote',
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: 'titleText',
+        class: 'titleText',
+        tag: 'h1',
+      },
+    ],
+};
+///
   constructor(private data : DataService , private http: HttpClient){
       http.get("https://anihmsu.comsciproject.net/anihmsu/api.php/records/information_anihmsu")
       .subscribe((data : any) => {
@@ -19,6 +69,30 @@ export class VMSPComponent {
         this.response = data as DataVetAnimal;
   });
 }
+////wysiwyg
+ngOnInit(): void {
+  let url = this.data.apiEndpoint + '/information_anihmsu';
+  this.http.get(url, {observe : 'response'}).subscribe(
+    {
+      next: (data :any)=>{
+        this.faculty_info = data.body.records[0];
+        this.htmlContent = this.faculty_info.vision_th;
+        this.htmlContent1 = this.faculty_info.vision_en;
+        this.htmlContent2 = this.faculty_info.mission_th;
+        this.htmlContent3 = this.faculty_info.mission_en;
+        this.htmlContent4 = this.faculty_info.strategy_th;
+        this.htmlContent5 = this.faculty_info.strategy_en;
+        this.htmlContent6 = this.faculty_info.agency_th;
+        this.htmlContent7 = this.faculty_info.agency_en;
+        console.log(this.faculty_info.id_anihmsu);
+      },
+      error: (err)=>{
+        console.log(err);
+      }
+    }
+  )
+}
+////
 getFile(files : FileList){
   let file = files.item(0);
   this.filename = file?.name;
@@ -32,21 +106,21 @@ getFile(files : FileList){
   //location.reload();
 
 }
-updateVMSP(vision_th : string , vision_en : string ,mission_th : string,mission_en : string,strategy_th : string ,strategy_en : string,agency_th : string,agency_en : string,idx : number){
+updateVMSP(){
   let jsonObj ={
     img_VMSA : this.base64,
-    vision_th : vision_th,
-    vision_en : vision_en,
-    mission_th : mission_th,
-    mission_en : mission_en,
-    strategy_th : strategy_th,
-    strategy_en : strategy_en,
-    agency_th : agency_th,
-    agency_en : agency_en
+    vision_th : this.htmlContent,
+    vision_en : this.htmlContent1,
+    mission_th : this.htmlContent2,
+    mission_en : this.htmlContent3,
+    strategy_th : this.htmlContent4,
+    strategy_en : this.htmlContent5,
+    agency_th : this.htmlContent6,
+    agency_en : this.htmlContent7
 
   }
   if (confirm("ยืนยันการเเก้ไข!") == true) {
-    this.http.put(this.data.apiEndpoint+"/information_anihmsu/"+idx,jsonObj,
+    this.http.put(this.data.apiEndpoint+"/information_anihmsu/"+this.faculty_info.id_anihmsu,jsonObj,
     {observe:'response'}).subscribe((response : any)=>
     {console.log(JSON.stringify(response.status));
       console.log(JSON.stringify(response.body));
